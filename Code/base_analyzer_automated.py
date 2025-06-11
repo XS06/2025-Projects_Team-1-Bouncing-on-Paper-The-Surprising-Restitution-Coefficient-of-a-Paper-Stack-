ed18b846e7c278ea572a78c312aa49831cf81161
S06 <saladin@shah.pm>
2025-06-11 15:57:28 +0200

    Added test file for nonstandard csv formats
    And added some debugging print statements. And found a way for an exact measurement of "ylim"

--- Code/base_analyzer_automated.py
+++ Code/base_analyzer_automated.py
@@ -7,14 +7,14 @@ import math as mt
 import matplotlib.pyplot as plt
 import csv as csv
 
-
+test_max_snelheid = []
+test_minimale_hoogte = []
 coefficienten_1_lijst = []
 coefficienten_2_lijst = []
 coefficienten_3_lijst = []
 
 # deze heeft alle huidige met data op 1 column
 # order_lijst = ["2-1", "3-1", "1-2", "2-2", "3-2", "1-4", "3-4", "1-6", "2-6", "3-6", "1-8", "3-8", "3-10", "1-12", "3-12", "1-14", "2-14", "3-14", "1-16", "2-16", "3-16", "1-30", "2-45", "2-60"]
-
 
 order_lijst = ["2-1", "2-2", "3-4", "2-6", "3-8", "3-10", "3-12", "2-14", "2-16", "1-30", "2-45", "2-60"]
 a_lijst = [1, 2, 3]
@@ -39,7 +39,7 @@ for o in order_lijst:
                         if data_opgeknipt and data_opgeknipt[0] != 'Track':
                             tijd = int(data_opgeknipt[0])
                             y1 = float(data_opgeknipt[2])
-                            hoogte = 660 - y1
+                            hoogte = 659.0764 - y1
 
                             tijd_lijst.append(tijd)
                             hoogte_lijst.append(hoogte)
@@ -63,7 +63,7 @@ for o in order_lijst:
         # Toppen detecteren: waar snelheid verandert van positief naar negatief
         maxima_tijden = [0]
         for i in range(1, len(snelheden)):
-            if snelheden[i - 1] > 0 and snelheden[i] < 0 and len(maxima_tijden) <= 3 and i > 50:  # filter op realistische waarde
+            if snelheden[i - 1] > 0 and snelheden[i] < 0 and len(maxima_tijden) <= 3 and i > 60:  # filter op realistische waarde
                 maxima_tijden.append(i)
 
         print(maxima_tijden)
@@ -72,7 +72,7 @@ for o in order_lijst:
         # Hoogtes van de toppen
         maxima_hoogtes = [hoogte_lijst[i] for i in maxima_tijden]
 
-        print(maxima_hoogtes)
+        # print(maxima_hoogtes)
 
 
         # Restitutiecoëfficiënten berekenen: hoogte_n / hoogte_(n-1)
@@ -85,17 +85,25 @@ for o in order_lijst:
         coefficienten_2_lijst.append(cor2)
         coefficienten_3_lijst.append(cor3)
 
+        plt.figure(1)
+        plt.plot(tijd_lijst, hoogte_lijst)
+        plt.xlabel("Time (frames)")
+        plt.ylabel('Height (px)')
+        # plt.ylim(0, 200)
+        # plt.show()
 
-        print(cor1, cor2, cor3)
+        test_max_snelheid.append(max(snelheden))
+        # print(cor1, cor2, cor3)
+        test_minimale_hoogte.append(min(hoogte_lijst))
 
 print(coefficienten_1_lijst)
 print(coefficienten_2_lijst)
 print(coefficienten_3_lijst)
 
-
+# papier_lijst = [1, 1, 2, 2, 2, 4, 4, 6, 6, 6, 8, 8, 10, 12, 12, 14, 14, 14, 16, 16, 16, 30, 45, 60]
 papier_lijst = [1, 2, 4, 6, 8, 10, 12, 14, 16, 30, 45, 60]
 
-plt.figure()
+plt.figure(2)
 plt.plot(papier_lijst, coefficienten_1_lijst, label='h1/h0 (first bounce)')
 plt.plot(papier_lijst, coefficienten_2_lijst, label='h2/h1 (second bounce)')
 plt.plot(papier_lijst, coefficienten_3_lijst, label='h3/h2 (third bounce)')
@@ -104,3 +112,6 @@ plt.xlabel('# of paper pages (amount)')
 plt.ylabel('Restitutioncoefficient (ratio)')
 plt.legend()
 plt.show()
+
+print(min(test_minimale_hoogte))
+print(max(test_max_snelheid))
