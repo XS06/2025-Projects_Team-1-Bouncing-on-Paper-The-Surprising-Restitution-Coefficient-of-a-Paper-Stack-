017e0cc4f79d087b7cb19e406cf4bd268518249c
NWitte84 <noor.witte@student.uva.nl>
2025-06-16 11:24:56 +0200

    Add files via upload

---
+++ Manim/animation_scenes.py
@@ -0,0 +1,134 @@
+from manim import *
+
+class Animation(Scene):
+    def construct(self):
+        # tekst
+        textboven = Text("Restitution Coefficient")
+        textboven.move_to(UP * 0.5)
+        textonder = Text("of a falling marble on a stack of paper")
+        textonder.move_to(DOWN * 0.5)
+
+        self.play(Write(textboven))
+        self.play(Write(textonder))
+        self.wait(2)
+        self.play(FadeOut(textboven),FadeOut(textonder))
+        
+
+        # knikker en papier maken en benoemen     
+        circle = Circle(radius = 0.5) 
+        circle.set_fill(BLUE,opacity=0.7)      
+        rectangular = Rectangle(width=4, height = 1)
+        rectangular.set_fill(WHITE, opacity=0.7)     
+        rectangular.to_edge(DOWN)
+        self.play(Create(rectangular))
+        
+        textpapier = Text("Stack of paper with x number of layers")
+        textpapier.move_to(UP * 0.5)
+        self.play(Write(textpapier))
+        arrow = Arrow (ORIGIN, [0,-2,0], buff = 0 )
+        self.play(Create(arrow))
+        self.play(FadeOut(arrow), FadeOut(textpapier))
+        circle.to_edge(UP)
+        self.play(Create(circle))
+        textknikker = Text("marble")
+        textknikker.move_to(DOWN * 0.5)
+        self.play(Write(textknikker))
+        arrow = Arrow(ORIGIN, [0,2,0], buff = 0)
+        self.play(Create(arrow))
+        self.play(FadeOut(arrow), FadeOut(textknikker))
+
+        # laten stuiteren van de knikker
+        self.play(circle.animate.shift(DOWN * 5), run_time = 1)
+        self.play(circle.animate.shift(UP * 4), run_time = 0.75)
+        self.play(circle.animate.shift(DOWN * 4), run_time = 0.75)
+        texthoogte = Text("The marble drops lower with each bounce")
+        texthoogte.move_to(UP * 3)
+        self.play(Write(texthoogte), circle.animate.shift(UP * 3), run_time = 0.5)
+        self.play(circle.animate.shift(DOWN * 3), run_time = 0.5)
+        self.play(circle.animate.shift(UP * 2), run_time = 0.25)
+        self.play(circle.animate.shift(DOWN * 2), run_time = 0.25)
+        self.play(circle.animate.shift(UP * 1), run_time = 0.125)
+        self.play(circle.animate.shift(DOWN * 1), run_time = 0.125)
+        self.play(circle.animate.shift(UP * 0.5), run_time = 0.06)
+        self.play(circle.animate.shift(DOWN * 0.5), run_time = 0.06)
+        self.play(circle.animate.shift(UP * 0.25), run_time = 0.03)
+        self.play(circle.animate.shift(DOWN * 0.25), run_time = 0.03)
+        self.play(FadeOut(texthoogte), FadeOut(circle), FadeOut(rectangular))
+    
+        # Uitleg CoR
+        textuitlegcordeel1 = Text("The ratio of")
+        textuitlegcordeel3 = Text("the height after and before a bounce")
+        textuitlegcordeel2 = Text("is equal to the restitution coefficient:")
+        textuitlegcordeel1.move_to(UP * 1)
+        textuitlegcordeel2.move_to(DOWN * 1)
+        self.play(FadeIn(textuitlegcordeel1), FadeIn(textuitlegcordeel2), FadeIn(textuitlegcordeel3))
+        self.wait(3)
+        self.play(FadeOut(textuitlegcordeel1), FadeOut(textuitlegcordeel2), FadeOut(textuitlegcordeel3))
+
+        # laten zien van CoR
+        circle = Circle(radius = 0.5) 
+        circle.set_fill(BLUE,opacity=0.7)      
+        rectangular = Rectangle(width=4, height = 1)
+        rectangular.set_fill(WHITE, opacity=0.7)     
+        rectangular.to_edge(DOWN)
+        self.play(Create(rectangular)) 
+        circle.to_edge(UP)
+        self.play(Create(circle))
+        texthoogte1 = Text("The height of the marble before the bounce")
+        texth1 = Text("(height 1)")
+        texth1.move_to(DOWN * 1)
+        lineh1 = Line([-0.5,2.5,0], [0.5,2.5,0]).set_color(WHITE)
+        self.play(Create(lineh1))
+        self.play(Write(texthoogte1))
+        self.play(Write(texth1))
+        self.play(FadeOut(texthoogte1), FadeOut(lineh1))
+        self.play(texth1.animate.shift(RIGHT * 4))
+        #self.play(texth1.animate.shift(DOWN * 1))
+        self.play(circle.animate.shift(DOWN*5))
+        self.play(circle.animate.shift(UP*3))
+        texthoogte2 = Text("The height of the marble after the bounce")
+        texth2 = Text("(height 2)")
+        texth2.move_to(DOWN*1)
+        lineh2 = Line([-0.5,0.5,0], [0.5, 0.5, 0]).set_color(WHITE)
+        self.play(Create(lineh2))
+        self.play(Write(texthoogte2))
+        self.play(Write(texth2))
+        self.play(FadeOut(texthoogte2), FadeOut(lineh2))
+        self.play(texth2.animate.shift(UP * 1.5))
+        self.play(texth2.animate.shift(RIGHT * 4))
+        #self.play(texth2.animate.shift(UP*1.5))
+        self.play(FadeOut(circle), FadeOut(rectangular))
+        self.play(texth1.animate.shift(LEFT*4), texth2.animate.shift(LEFT*4))
+        linebreuk1 = Line([1,0,0], [-1,0,0]).set_color(WHITE)
+        self.play(Create(linebreuk1), texth1.animate.shift(UP*0.5))
+        self.play(texth1.animate.shift(LEFT * 3.5), texth2.animate.shift(LEFT * 3.5), linebreuk1.animate.shift(LEFT * 3.5))
+        lineis1 = Line([-1.7,0.15,0], [-0.7,0.15,0]).set_color(WHITE)
+        lineis2 = Line([-1.7,-0.2,0], [-0.7,-0.2,0]).set_color(WHITE)
+        self.play(Create(lineis1), Create(lineis2))
+
+        textboven.move_to(RIGHT*2.5)
+        self.play(Write(textboven))
+        self.play(FadeOut(textboven), FadeOut(texth1), FadeOut(texth2), FadeOut(lineis1), FadeOut(lineis2))
+
+        # ax = Axes(
+        #     x_range=[0,500, 50], y_range=[0,1,0.1], x_axis_config= {"numbers_to_include":[]})
+        # #labels = ax.get_axis_labels(x_label ="aantal vellen papier", y_label = "Resitutie Coëfficient (CoR)")
+        # self.add(ax)
+
+        # assen = Axes(
+        #     x_range=[-3, 3, 1],   # [min, max, stapgrootte]
+        #     y_range=[-1, 9, 1],
+        #     axis_config={"color": BLUE},
+        #     x_axis_config={"numbers_to_include": [-2, 0, 2]},
+        #     y_axis_config={"numbers_to_include": [0, 4, 8]},
+        # )
+
+        #labels = assen.get_axis_labels(x_label="x", y_label="f(x)")
+
+        #functie = assen.plot(lambda x: x**2, color=YELLOW)
+
+        #functielabel = assen.get_graph_label(functie, label="x^2")
+
+        #self.play(Create(assen))
+        #self.play(Create(functie))
+       #
