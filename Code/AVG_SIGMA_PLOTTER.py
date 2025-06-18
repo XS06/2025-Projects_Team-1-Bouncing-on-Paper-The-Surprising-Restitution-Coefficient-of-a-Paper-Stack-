390006a9a12f74641f2e3c11b0615be79a6fa978
S06 <saladin@shah.pm>
2025-06-18 21:20:15 +0200

    Added python code to extract sanitized data and plot the average with standard deviation

---
+++ Code/AVG_SIGMA_PLOTTER.py
@@ -0,0 +1,163 @@
+# Naam: Saladin Shah
+# Studentnummer: 15817490
+
+
+import numpy as np
+import math as mt
+import matplotlib.pyplot as plt
+import csv as csv
+import statistics as stat
+
+
+A4_CSV = open('E:/Documents/GitHub/2025-Projects_Team-1-Bouncing-on-Paper-The-Surprising-Restitution-Coefficient-of-a-Paper-Stack-/Code/A4_sanitized_data.csv', 'r')
+A5_B_CSV = open('E:/Documents/GitHub/2025-Projects_Team-1-Bouncing-on-Paper-The-Surprising-Restitution-Coefficient-of-a-Paper-Stack-/Code/A5_B_sanitized_data.csv', 'r')
+A5_O_CSV = open('E:/Documents/GitHub/2025-Projects_Team-1-Bouncing-on-Paper-The-Surprising-Restitution-Coefficient-of-a-Paper-Stack-/Code/A5_O_sanitized_data.csv', 'r')
+
+with A4_CSV as A4:
+    A4_papier_lijst = []
+    A4_coefficienten_1_h_lijst = []
+    A4_fout_coefficienten_1_h_lijst = []
+    A4_coefficienten_1_v_lijst = []
+    A4_fout_coefficienten_1_v_lijst = []
+
+    A4_compressed_papier_lijst = []
+    A4_avg_h_lijst = []
+    A4_sigma_h_lijst = []
+    A4_avg_v_lijst = []
+    A4_sigma_v_lijst = []
+
+    for row in A4:
+        column = row.split(',')
+
+        if column[0] != 'A4_papier_lijst':
+            A4_papier_lijst.append(int(column[0]))
+            A4_coefficienten_1_h_lijst.append(float(column[1]))
+            A4_fout_coefficienten_1_h_lijst.append(float(column[2]))
+            A4_coefficienten_1_v_lijst.append(float(column[3]))
+            A4_fout_coefficienten_1_v_lijst.append(float(column[4]))
+    
+    for papier in range(0, int(len(A4_papier_lijst) - 2)):
+        if A4_papier_lijst[papier] == A4_papier_lijst[papier + 1] == A4_papier_lijst[papier + 2]:
+            A4_compressed_papier_lijst.append(A4_papier_lijst[papier])
+            A4_temp_h_list = [A4_coefficienten_1_h_lijst[papier], A4_coefficienten_1_h_lijst[papier + 1], A4_coefficienten_1_h_lijst[papier + 2]]
+            A4_avg_h_lijst.append(float(np.mean(A4_temp_h_list)))
+            A4_sigma_h_lijst.append(float(np.std(A4_temp_h_list)))
+            A4_temp_v_list = [A4_coefficienten_1_v_lijst[papier], A4_coefficienten_1_v_lijst[papier + 1], A4_coefficienten_1_v_lijst[papier + 2]]
+            A4_avg_v_lijst.append(float(np.mean(A4_temp_v_list)))
+            A4_sigma_v_lijst.append(float(np.std(A4_temp_v_list)))
+        
+
+    A4_CSV.close()
+
+
+with A5_B_CSV as A5_B:
+    A5_papier_lijst = []
+    A5_coefficienten_1_h_B_lijst = []
+    A5_fout_coefficienten_1_h_B_lijst = []
+    A5_coefficienten_1_v_B_lijst = []
+    A5_fout_coefficienten_1_v_B_lijst = []
+
+    A5_compressed_papier_lijst = []
+    A5_avg_h_B_lijst = []
+    A5_sigma_h_B_lijst = []
+    A5_avg_v_B_lijst = []
+    A5_sigma_v_B_lijst = []
+
+    for row in A5_B:
+        column = row.split(',')
+
+        if column[0] != 'A5_papier_lijst':
+            A5_papier_lijst.append(int(column[0]))
+            A5_coefficienten_1_h_B_lijst.append(float(column[1]))
+            A5_fout_coefficienten_1_h_B_lijst.append(float(column[2]))
+            A5_coefficienten_1_v_B_lijst.append(float(column[3]))
+            A5_fout_coefficienten_1_v_B_lijst.append(float(column[4]))
+    
+    for papier in range(0, int(len(A5_papier_lijst) - 1)):
+        if A5_papier_lijst[papier] == A5_papier_lijst[papier + 1]:
+            A5_compressed_papier_lijst.append(A5_papier_lijst[papier])
+            A5_temp_h_B_list = [A5_coefficienten_1_h_B_lijst[papier], A5_coefficienten_1_h_B_lijst[papier + 1]]
+            A5_avg_h_B_lijst.append(float(np.mean(A5_temp_h_B_list)))
+            A5_sigma_h_B_lijst.append(float(np.std(A5_temp_h_B_list)))
+            A5_temp_v_B_list = [A5_coefficienten_1_v_B_lijst[papier], A5_coefficienten_1_v_B_lijst[papier + 1]]
+            A5_avg_v_B_lijst.append(float(np.mean(A5_temp_v_B_list)))
+            A5_sigma_v_B_lijst.append(float(np.std(A5_temp_v_B_list)))
+        
+
+    A5_B_CSV.close()
+
+
+with A5_O_CSV as A5_O:
+    A5_papier_lijst = []
+    A5_coefficienten_1_h_O_lijst = []
+    A5_fout_coefficienten_1_h_O_lijst = []
+    A5_coefficienten_1_v_O_lijst = []
+    A5_fout_coefficienten_1_v_O_lijst = []
+
+    A5_compressed_papier_lijst = []
+    A5_avg_h_O_lijst = []
+    A5_sigma_h_O_lijst = []
+    A5_avg_v_O_lijst = []
+    A5_sigma_v_O_lijst = []
+
+    for row in A5_O:
+        column = row.split(',')
+
+        if column[0] != 'A5_papier_lijst':
+            A5_papier_lijst.append(int(column[0]))
+            A5_coefficienten_1_h_O_lijst.append(float(column[1]))
+            A5_fout_coefficienten_1_h_O_lijst.append(float(column[2]))
+            A5_coefficienten_1_v_O_lijst.append(float(column[3]))
+            A5_fout_coefficienten_1_v_O_lijst.append(float(column[4]))
+    
+    for papier in range(0, int(len(A5_papier_lijst) - 1)):
+        if A5_papier_lijst[papier] == A5_papier_lijst[papier + 1]:
+            A5_compressed_papier_lijst.append(A5_papier_lijst[papier])
+            A5_temp_h_O_list = [A5_coefficienten_1_h_O_lijst[papier], A5_coefficienten_1_h_O_lijst[papier + 1]]
+            A5_avg_h_O_lijst.append(float(np.mean(A5_temp_h_O_list)))
+            A5_sigma_h_O_lijst.append(float(np.std(A5_temp_h_O_list)))
+            A5_temp_v_O_list = [A5_coefficienten_1_v_O_lijst[papier], A5_coefficienten_1_v_O_lijst[papier + 1]]
+            A5_avg_v_O_lijst.append(float(np.mean(A5_temp_v_O_list)))
+            A5_sigma_v_O_lijst.append(float(np.std(A5_temp_v_O_list)))
+        
+
+    A5_O_CSV.close()
+
+
+plt.figure(1, figsize=(15, 5))
+plt.suptitle('A4 and A5 format combined. Restitutioncoefficient, calculated with h1/h0 and v1/v0. Average of all measurements with standard deviation')
+plt.subplot(121)
+plt.title('h1/h0 calculation')
+plt.errorbar(A4_compressed_papier_lijst, A4_avg_h_lijst, yerr=A4_sigma_h_lijst, capsize=3, fmt='o', ecolor = "black", label='A4 data and standard deviation', markersize='5')
+plt.errorbar(A5_compressed_papier_lijst, A5_avg_h_B_lijst, yerr=A5_sigma_h_B_lijst, capsize=3, fmt='o', ecolor = "black", label='A5 data and standard deviation', markersize='5')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+plt.legend()
+plt.subplot(122)
+plt.title('v1/v0 calculation')
+plt.errorbar(A4_compressed_papier_lijst, A4_avg_v_lijst, yerr=A4_sigma_v_lijst, capsize=3, fmt='o', ecolor = "black", label='A4 data and standard deviation', markersize='5')
+plt.errorbar(A5_compressed_papier_lijst, A5_avg_v_B_lijst, yerr=A5_sigma_v_B_lijst, capsize=3, fmt='o', ecolor = "black", label='A5 data and standard deviation', markersize='5')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+plt.legend()
+plt.savefig('A4_AND_A5_COR_AVG_SIGMA')
+
+
+plt.figure(2, figsize=(15, 5))
+plt.suptitle('A5 format, dropped from half the height of original measurements. Restitutioncoefficient, calculated with h1/h0 and v1/v0. Average of all measurements with standard deviation')
+plt.subplot(221)
+plt.title('h1/h0 calculation')
+plt.errorbar(A5_compressed_papier_lijst, A5_avg_h_O_lijst, yerr=A5_sigma_h_O_lijst, capsize=3, fmt='o', ecolor = "black", label='A5', markersize='5', color='darkorange')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+plt.legend()
+plt.subplot(222)
+plt.title('v1/v0 calculation')
+plt.errorbar(A5_compressed_papier_lijst, A5_avg_v_O_lijst, yerr=A5_sigma_v_O_lijst, capsize=3, fmt='o', ecolor = "black", label='A5', markersize='5', color='darkorange')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+plt.legend()
+plt.savefig('A5_COR_LOW_AVG_SIGMA')
+
+
+plt.show()
