7dc1eee2fc2c3b436695189c7a4ba9d1728479d8
S06 <saladin@shah.pm>
2025-06-23 14:45:21 +0200

    Separated and added graphs for clarity, fixed error of double appending, final final final final final graphs

--- Code/AVG_SIGMA_PLOTTER.py
+++ Code/AVG_SIGMA_PLOTTER.py
@@ -56,20 +56,22 @@ with A4_CSV as A4:
             if A4_papier_lijst[papier - 2] == A4_papier_lijst[papier - 1 ] == A4_papier_lijst[papier]:
                 print('papier', papier, A4_papier_lijst[papier], A4_papier_lijst[papier - 1])
             else:
-                print(papier, A4_papier_lijst[papier], A4_papier_lijst[papier + 1])
-                A4_compressed_papier_lijst.append(A4_papier_lijst[papier])
-                A4_temp_h_list = [A4_coefficienten_1_h_lijst[papier], A4_coefficienten_1_h_lijst[papier + 1]]
-                A4_fout_temp_h_list = [A4_fout_coefficienten_1_h_lijst[papier], A4_fout_coefficienten_1_h_lijst[papier + 1]]
-                A4_avg_h_lijst.append(float(np.mean(A4_temp_h_list)))
-                A4_fout_h_lijst.append(float(np.sqrt(np.std(A4_temp_h_list) + np.var(A4_fout_temp_h_list))))
-                A4_temp_v_list = [A4_coefficienten_1_v_lijst[papier]]
-                A4_fout_temp_v_list = [A4_fout_coefficienten_1_v_lijst[papier]]
-                A4_avg_v_lijst.append(float(np.mean(A4_temp_v_list)))
-                A4_fout_v_lijst.append(float(np.sqrt(np.std(A4_temp_h_list) + np.var(A4_fout_temp_v_list))))
-        
+                if A4_papier_lijst[papier - 2] != A4_papier_lijst[papier - 1 ] and A4_papier_lijst[papier] != A4_papier_lijst[papier + 1]:  
+                    print(papier, A4_papier_lijst[papier], A4_papier_lijst[papier + 1])
+                    A4_compressed_papier_lijst.append(A4_papier_lijst[papier])
+                    A4_temp_h_list = [A4_coefficienten_1_h_lijst[papier], A4_coefficienten_1_h_lijst[papier - 1]]
+                    A4_fout_temp_h_list = [A4_fout_coefficienten_1_h_lijst[papier], A4_fout_coefficienten_1_h_lijst[papier - 1]]
+                    A4_avg_h_lijst.append(float(np.mean(A4_temp_h_list)))
+                    A4_fout_h_lijst.append(float(np.sqrt(np.std(A4_temp_h_list) + np.var(A4_fout_temp_h_list))))
+                    A4_temp_v_list = [A4_coefficienten_1_v_lijst[papier]]
+                    A4_fout_temp_v_list = [A4_fout_coefficienten_1_v_lijst[papier]]
+                    A4_avg_v_lijst.append(float(np.mean(A4_temp_v_list)))
+                    A4_fout_v_lijst.append(float(np.sqrt(np.std(A4_temp_h_list) + np.var(A4_fout_temp_v_list))))
+    
 
     A4_CSV.close()
 
+print(A4_compressed_papier_lijst)
 
 with A5_B_CSV as A5_B:
     A5_papier_lijst = []
@@ -166,16 +168,16 @@ with A5_O_CSV as A5_O:
 print(len(A5_compressed_papier_lijst))
 print(len(A5_avg_h_B_lijst))
 
-plt.figure(1, figsize=(15, 5))
-plt.suptitle('A4 and A5 format combined. Restitutioncoefficient, calculated using height difference and using velocity difference. Average of all measurements')
-plt.subplot(121)
+plt.figure(1, figsize=(10, 10))
 plt.title('Height calculation, sqrt(h1/h0)')
 plt.errorbar(A4_compressed_papier_lijst, A4_avg_h_lijst, yerr=A4_fout_h_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A4 data', markersize='3')
 plt.errorbar(A5_compressed_papier_lijst, A5_avg_h_B_lijst, yerr=A5_fout_h_B_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A5 data', markersize='3')
 plt.xlabel('# of paper pages (amount)')
 plt.ylabel('Restitutioncoefficient (ratio)')
 plt.legend()
-plt.subplot(122)
+plt.savefig('A4_AND_A5_COR_H_AVG_SIGMA')
+
+plt.figure(2, figsize=(10, 10))
 plt.title('Velocity calculation, v1/v0')
 plt.errorbar(A4_compressed_papier_lijst, A4_avg_v_lijst, yerr=A4_fout_v_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A4 data', markersize='3')
 plt.errorbar(A5_compressed_papier_lijst, A5_avg_v_B_lijst, yerr=A5_fout_v_B_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A5 data', markersize='3')
@@ -183,28 +185,62 @@ plt.xlabel('# of paper pages (amount)')
 plt.ylabel('Restitutioncoefficient (ratio)')
 # plt.ylim(0.1, 0.8)
 plt.legend()
-plt.savefig('A4_AND_A5_COR_AVG_SIGMA')
+plt.savefig('A4_AND_A5_COR_V_AVG_SIGMA')
 
+plt.figure(3, figsize=(10, 10))
+plt.title('Height calculation, sqrt(h1/h0)')
+plt.errorbar(A5_compressed_papier_lijst, A5_avg_h_B_lijst, yerr=A5_fout_h_B_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A5', markersize='3', color='darkorange')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+plt.legend()
+plt.savefig('A5_COR_H_HIGH_AVG_SIGMA')
 
-plt.figure(2, figsize=(20, 10))
-plt.suptitle('A5 format, dropped from half the height of original measurements. Restitutioncoefficient, calculated using height difference and using velocity difference. Average of all measurements')
-plt.subplot(221)
+plt.figure(4, figsize=(10, 10))
+plt.title('Velocity calculation, v1/v0')
+plt.errorbar(A5_compressed_papier_lijst, A5_avg_v_B_lijst, yerr=A5_fout_v_B_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A5', markersize='3', color='darkorange')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+# plt.ylim(0.4, 0.7)
+plt.legend()
+plt.savefig('A5_COR_V_HIGH_AVG_SIGMA')
+
+plt.figure(5, figsize=(10, 10))
 plt.title('Height calculation, sqrt(h1/h0)')
 plt.errorbar(A5_compressed_papier_lijst, A5_avg_h_O_lijst, yerr=A5_fout_h_O_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A5', markersize='3', color='darkorange')
 plt.xlabel('# of paper pages (amount)')
 plt.ylabel('Restitutioncoefficient (ratio)')
 plt.legend()
-plt.subplot(222)
+plt.savefig('A5_COR_H_LOW_AVG_SIGMA')
+
+plt.figure(6, figsize=(10, 10))
 plt.title('Velocity calculation, v1/v0')
 plt.errorbar(A5_compressed_papier_lijst, A5_avg_v_O_lijst, yerr=A5_fout_v_O_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A5', markersize='3', color='darkorange')
 plt.xlabel('# of paper pages (amount)')
 plt.ylabel('Restitutioncoefficient (ratio)')
 # plt.ylim(0.4, 0.7)
 plt.legend()
-plt.savefig('A5_COR_LOW_AVG_SIGMA')
+plt.savefig('A5_COR_V_LOW_AVG_SIGMA')
 
 
-# plt.figure(3, figsize=(15, 5))
+plt.figure(7, figsize=(10, 10))
+plt.title('Height calculation, sqrt(h1/h0)')
+plt.errorbar(A4_compressed_papier_lijst, A4_avg_h_lijst, yerr=A4_fout_h_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A4 data', markersize='3')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+plt.legend()
+plt.savefig('A4_COR_H_AVG_SIGMA')
+
+plt.figure(8, figsize=(10, 10))
+plt.title('Velocity calculation, v1/v0')
+plt.errorbar(A4_compressed_papier_lijst, A4_avg_v_lijst, yerr=A4_fout_v_lijst, capsize=2, fmt='o', ecolor = "#dddddd", label='A4 data', markersize='3')
+plt.xlabel('# of paper pages (amount)')
+plt.ylabel('Restitutioncoefficient (ratio)')
+# plt.ylim(0.1, 0.8)
+plt.legend()
+plt.savefig('A4_COR_V_AVG_SIGMA')
+
+
+# plt.figure(3, figsize=(10, 10))
 # plt.subplot(321)
 # # plt.plot(A4_compressed_papier_lijst, A4_avg_h_lijst, 'o')
 # plt.plot(A4_papier_lijst, A4_coefficienten_1_h_lijst, 'o')
@@ -214,7 +250,7 @@ plt.savefig('A5_COR_LOW_AVG_SIGMA')
 # # plt.plot(A4_compressed_papier_lijst, A4_avg_v_lijst, 'o')
 # plt.plot(A4_papier_lijst, A4_coefficienten_1_v_lijst, 'o')
 
-# plt.figure(4, figsize=(15, 5))
+# plt.figure(4, figsize=(10, 10))
 # plt.subplot(421)
 # # plt.errorbar(A5_papier_lijst, A5_coefficienten_1_h_B_lijst, yerr=A5_fout_coefficienten_1_h_B_lijst, capsize=3, fmt='o', ecolor = "#dddddd", label='A5', markersize='5', color='darkorange')
 # plt.plot(A5_papier_lijst, A5_coefficienten_1_h_B_lijst, 'o')
@@ -222,7 +258,7 @@ plt.savefig('A5_COR_LOW_AVG_SIGMA')
 # # plt.errorbar(A5_papier_lijst, A5_coefficienten_1_v_B_lijst, yerr=A5_fout_coefficienten_1_v_B_lijst, capsize=3, fmt='o', ecolor = "#dddddd", label='A5', markersize='5', color='darkorange')
 # plt.plot(A5_papier_lijst, A5_coefficienten_1_v_B_lijst, 'o')
 
-# plt.figure(5, figsize=(15, 5))
+# plt.figure(5, figsize=(10, 10))
 # plt.subplot(521)
 # # plt.plot(A5_papier_lijst, A5_coefficienten_1_h_O_lijst, 'o')
 # # plt.errorbar(A5_papier_lijst, A5_coefficienten_1_h_O_lijst, yerr=A5_fout_coefficienten_1_h_O_lijst, capsize=3, fmt='o', ecolor = "#dddddd", label='A5', markersize='5', color='darkorange')
